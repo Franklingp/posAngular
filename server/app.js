46c6053a0131165a2114831421fdbb83c8b16e1b
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2019-06-05 22:15:05 -0500

    Ya estan instaladas dependencias del backend y la base de datos ya esta enlazada

---
+++ server/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//Configuraciones del servicio
+
+var express = require('express');
+var body_parser = require('body-parser');
+var app = express();
+
+//Archivos de rutas
+
+
+//Middlewares
+
+
+// Configurar cabeceras y cors
+
+
+
+//Rutas
+
+
+//Exportar
+
+module.exports = app;
