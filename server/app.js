075ee8532bbe1ca3d65dbd8bc304905a55bc67c1
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2019-06-05 22:19:37 -0500

    agregado los middleware de body-parser

--- server/app.js
+++ server/app.js
@@ -3,14 +3,15 @@
 //Configuraciones del servicio
 
 var express = require('express');
-var body_parser = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
 //Archivos de rutas
 
 
 //Middlewares
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // Configurar cabeceras y cors
 
